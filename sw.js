0e9456b42c5999c7ec32a9c5d248fb151e37cc7f
billi-cyber <philipp.sch95@outlook.com>
2026-06-25 08:03:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Loop Trainer Service Worker — enables full offline use
-var CACHE_NAME = 'loop-trainer-v44';
+var CACHE_NAME = 'loop-trainer-v45';
 var URLS_TO_CACHE = [
   './',
   './index.html',
